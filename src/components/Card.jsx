f493a0bfcaf23f63e348f2bdc987df52842989a1
ConanGH-S <blandon0207s@gmail.com>
2022-10-11 21:39:37 -0500

    Primer card React

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+const Card = () => {
+	return (
+		<div className="card-container">
+			<h2>Title</h2>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, accusantium.</p>
+			<button>Mostrar más</button>
+		</div>
+	);
+};
+
+export { Card };
